3ffdc13849b978a5405e83aa2a311d4a7c599fe7
Ana <analuizadiasdarocha212@gmail.com>
2023-08-11 14:48:45 -0300

    Aula 25 - Inserção do ItensCompra

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Autor, Categoria, Editora, Livro, Compra
+from .models import Autor, Categoria, Editora, Livro, Compra, ItensCompra
 
 admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
 admin.site.register(Compra)
+admin.site.register(ItensCompra)
